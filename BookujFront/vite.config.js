0d90301d60523e37c05b5744175d4ff0640e50dd
unknown <piotrekduleba@interia.pl>
2025-12-29 13:44:15 +0100

    Modified for production

--- BookujFront/vite.config.js
+++ BookujFront/vite.config.js
@@ -3,13 +3,23 @@ import react from "@vitejs/plugin-react";
 import fs from "fs";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync("./certs/localhost-key.pem"),
-      cert: fs.readFileSync("./certs/localhost.pem"),
-    },
-    port: 3000,
-  },
+export default defineConfig(({ command }) => {
+  if (command === "serve") {
+    // dev server
+    return {
+      plugins: [react()],
+      server: {
+        https: {
+          key: fs.readFileSync("./certs/localhost-key.pem"),
+          cert: fs.readFileSync("./certs/localhost.pem"),
+        },
+      },
+    };
+  } else {
+    // production build
+    return {
+      plugins: [react()],
+      // żadnego https w buildzie
+    };
+  }
 });
