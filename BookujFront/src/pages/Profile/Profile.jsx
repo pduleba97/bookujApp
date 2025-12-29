6377f6b03cf4de01450f6c6d65bcfd477bc531da
unknown <piotrekduleba@interia.pl>
2025-12-29 13:45:49 +0100

    Modified for production2

--- BookujFront/src/pages/Profile/Profile.jsx
+++ BookujFront/src/pages/Profile/Profile.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { authFetch } from "../../api/authFetch";
-import "./profile.css";
+import "./Profile.css";
 import { setAccessToken } from "../../api/sessionApi";
 import { reduceImageSize } from "../../utils/imageUtils";
 
