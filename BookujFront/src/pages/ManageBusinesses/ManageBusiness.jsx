47a954d428136f39453f6cfd37b0e12e169b94c8
pduleba97 <piotrekduleba@interia.pl>
2026-01-15 14:49:27 +0100

    Fix for leaflet marker icon not working on vercel

--- BookujFront/src/pages/ManageBusinesses/ManageBusiness.jsx
+++ BookujFront/src/pages/ManageBusinesses/ManageBusiness.jsx
@@ -17,8 +17,20 @@ import { useRef } from "react";
 import { useMap } from "react-leaflet";
 import ServiceModal from "../../components/ViewBusiness/ServiceModal";
 import BusinessPhotosGallery from "../BusinessDashboard/BusinessSettings/BusinessDetails/BusinessPhotos/BusinessPhotosGallery";
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 function ManageBusiness() {
+  delete L.Icon.Default.prototype._getIconUrl;
+
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: markerIcon2x,
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+  });
+
   const [address, setAddress] = useState(null);
   const [businessData, setBusinessData] = useState({
     id: "",
